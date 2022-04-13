e011a5b6ec4c9f977db18ff187b1dc3a2f2e4a89
Markus <markus.noelle@gmail.com>
2022-04-13 11:15:48 +0200

    Reasonably well-working layout, without VIA

--- keyboards/crkbd/keymaps/MartyBeGood/config.h
+++ keyboards/crkbd/keymaps/MartyBeGood/config.h
@@ -29,9 +29,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define EE_HANDS
 
 //#define TAPPING_FORCE_HOLD
-#define TAPPING_TERM 200
-#define TAPPING_FORCE_HOLD_PER_KEY
-
+//#define TAPPING_TERM 100
 
 #ifdef RGBLIGHT_ENABLE
     #define RGBLIGHT_EFFECT_BREATHING
