9b39d294a4f708eead668ab13340c5520b204497
Markus <markus.noelle@gmail.com>
2022-05-10 00:54:11 +0200

    Minor bugfixes

--- keyboards/crkbd/keymaps/MartyBeGood/keymap.c
+++ keyboards/crkbd/keymaps/MartyBeGood/keymap.c
@@ -32,6 +32,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 bool get_tapping_force_hold(uint16_t keycode, keyrecord_t* record) {
     switch (keycode) {
         case RAI_ENT:
+        case RAI_SPC:
             return true;
         default:
             return false;
